ec831bf2d76c5372670e994529e0275aabda188b
yabindra-bhujel <yabindra2057@gmail.com>
2024-04-03 12:14:12 +0900

    add env

--- bin/start_local_w.sh
+++ bin/start_local_w.sh
@@ -1,12 +1,5 @@
 @echo off
-echo Stopping processes on ports 3000 and 8000
 
-:kill_processes_on_port
-set "port=%~1"
-echo Killing processes on port %port%
-for /f "tokens=2" %%a in ('netstat -aon ^| findstr /r /c:"%port%"') do (
-    taskkill /f /pid %%a
-)
 
 echo Processes on ports 3000 and 8000 stopped
 echo Starting local development environment
@@ -22,15 +15,15 @@ cd ..
 
 cd api
 
-if exist "venv" (
-    echo Activating existing virtual environment
-    call venv\Scripts\activate
-) else (
-    echo Creating virtual environment
-    python -m venv venv
-    call venv\Scripts\activate
-    pip install -r requirements.txt
+
+echo Creating virtual environment
+# if already exists, delete it
+if exist venv (
+    rmdir /s /q venv
 )
+python -m venv venv
+call venv\Scripts\activate
+pip install -r requirements.txt
 
 echo Starting Django server
 python manage.py runserver
