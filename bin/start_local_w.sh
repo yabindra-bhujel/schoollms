0ec64fac4b41f7cbe67e99f2a05b65d76fb77c11
yabindra-bhujel <yabindra2057@gmail.com>
2024-04-03 11:38:30 +0900

    add start scprit for wo

---
+++ bin/start_local_w.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+echo "Stopping processes on ports 3000 and 8000"
+
+kill_processes_on_port() {
+    port=$1
+    echo "Killing processes on port $port"
+    pid=$(lsof -t -i:$port)
+    if [ -n "$pid" ]; then
+        kill -9 $pid
+    fi
+}
+kill_processes_on_port 3000
+kill_processes_on_port 8000
+kill_processes_on_port 3001
+
+echo "Processes on ports 3000 and 8000 stopped"
+echo "Starting local development environment"
+
+cd frontend
+npm install
+
+npm start &
+
+echo "Local development environment started"
+
+cd ../api
+
+pip install -r requirements.txt
+
+echo "Starting Django server"
+python3 manage.py runserver
+
+
